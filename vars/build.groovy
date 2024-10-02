cad31bd76cd9c89d8bc5fc6d6ad943accc23f31c
nazat mustag <nazat.mf@gmail.com>
2024-10-02 09:09:10 +0600

    add

--- vars/build.groovy
+++
@@ -1,3 +0,0 @@
-def call(String projectName,String imageTag, String dockerhubUser){
-    sh "docker build -t ${dockerhubUser}/${projectName}:${imageTag} ."
-}
--- vars/dockerpush.groovy
+++
@@ -1,6 +0,0 @@
-def call(String projectName,String imageTag){
-    withCredentials([usernamePassword('credentialsId':"dockerhub-token",passwordVariable:"dockerhubPass",usernameVariable:"dockerhubUser")]){
-            sh "docker login -u ${env.dockerhubUser} -p ${env.dockerhubPass}"
-            sh "docker push ${env.dockerhubUser}/${projectName}:${imageTag}"
-        }
-}
